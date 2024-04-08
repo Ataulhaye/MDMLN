68d59613d27033a2b031a8b6ead315dd9de61865
Haye <aahmad@daenet.com>
2024-04-08 14:56:51 +0200

    matlab stuff

---
+++ matlab/startinfo.py
@@ -0,0 +1,9 @@
+# Matlab stuff
+# How to make .nii file from the .mat file
+# https://de.mathworks.com/matlabcentral/fileexchange/8797-tools-for-nifti-and-analyze-image
+# nii_image = load("C:\Users\ataul\source\Uni\BachelorThesis\poc\All_Brain_Data_Raw\AAL_all_lobes_ROI.rex.mat")
+# matrix = cell2mat(struct2cell(nii_image))
+# nii_image1 = make_nii(matrix)
+# save_nii(nii_image1, "m1.nii")
+# how to make gz file
+# gzip("C:\Users\ataul\source\Uni\BachelorThesis\poc\All_Brain_Data_Raw\m1.nii")
