7dd8edef1eb953dd9a98cda9bd260fab76129b9c
Haye <aahmad@daenet.com>
2024-05-30 16:22:51 +0200

    code clean

--- main.py
+++ main.py
@@ -864,9 +864,6 @@ def main():
     analyser.RSA_related_unrelated_RDM()
 
     analyser = FMRIAnalyser(Lobe.IFG)
-    analyser.unary_subject_binary_image_classification_RSA_Test()
-
-    analyser = FMRIAnalyser(Lobe.IFG)
     analyser.training_config.dimension_reduction = True
     analyser.training_config.folds = 2
     # analyser.strategies.insert(0, "mice")
