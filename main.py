9f9ea6d3f1e4a4cb79f72a77cea34144cb9f1e52
Haye <aahmad@daenet.com>
2024-07-09 12:39:16 +0200

    working on MCQ features extraction

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import csv
+import glob
 import os
 from functools import partial
+from pathlib import Path
 
+import moviepy.editor as mp
+import pandas as pd
 import scipy.stats as stats
 import torch
 import torch.nn as nn
@@ -865,8 +870,105 @@ def generate_text_file_from_videos():
     vt.video_to_audio_transcript()
 
 
+# "C:\Users\ataul\source\Uni\BachelorThesis\poc\AudioTranscriptsSet1_EN.csv"
+def get_set_files(set_path, set_name):
+    set_keys = []
+    with open(set_path) as f:
+        for line in f:
+            k = line.split(" ")[0]
+            assert k.split(".")[1].replace('"', "") in "mpg"
+            set_keys.append(k)
+
+    all_scripts = r"C:\Users\ataul\source\Uni\BachelorThesis\poc\Result_Files\AudioTranscripts_corrected.csv"
+
+    set_transcripts = []
+    scripts_d = []
+    with open(all_scripts) as f:
+        scripts_d = f.readlines()
+
+    for key in set_keys:
+        for script in scripts_d:
+            if key in script:
+                set_transcripts.append(script)
+                break
+
+    assert len(set_transcripts) == len(set_keys)
+    dest_file = f"AudioTranscriptsSet{set_name}.csv"
+
+    f = open(dest_file, "w")
+    for trans in set_transcripts:
+        f.write(trans)
+    f.close()
+
+
+def copy_en_txt(source_en_file, source_de_file, set_name):
+    de_txt = None
+    with open(source_de_file) as f:
+        de_txt = f.readlines()
+
+    en_txt = None
+    with open(source_en_file) as f:
+        en_txt = f.readlines()
+
+    assert len(en_txt) == len(de_txt)
+
+    set_transcripts = []
+
+    for txt in de_txt:
+        de_en = get_translation(txt, en_txt)
+        if de_en is not None:
+            set_transcripts.append(de_en)
+
+    assert len(set_transcripts) == len(de_txt)
+
+    dest_file = f"AudioTranscriptsSet{set_name}_DE_EN.csv"
+
+    f = open(dest_file, "w")
+    for trans in set_transcripts:
+        f.write(trans)
+    f.close()
+
+
+def get_translation(txt, en_txt):
+    de_en = None
+    for script in en_txt:
+        s_script = script.strip("\n")
+        txt_seg = txt.split(";")
+        if txt_seg[0] in script:
+            de_en = txt.strip("\n") + ";" + s_script.split(";")[1].strip(" ") + "\n"
+            break
+    if de_en is None:
+        print("This must not happened")
+    return de_en
+
+
+def load_embeddings():
+    path = r"C:\Users\ataul\source\Uni\BachelorThesis\poc\PickleFiles\MCQ_Embeddings_Set1.pickle"
+    embeddings = pd.read_pickle(path)
+    for embed in embeddings:
+        key, text_embed, answer_embed, bridge_embed, vid_embed, en_de_txt = embed
+        print(key)
+    print("End")
+
+
+def mpg_to_mp4():
+    path = r"C:\Users\ataul\source\Uni\BachelorThesis\Stimuli"
+    directory_path = Path("Stimuli_mp4").absolute().resolve()
+    Path(directory_path).mkdir(parents=True, exist_ok=True)
+    for video_file_path in glob.glob(os.path.join(path, "*.mpg")):
+        file_name = video_file_path.split("\\")[-1].split(".")[0]
+        final_file_name = f"{file_name}.mp4"
+        save_path = os.path.join(directory_path, final_file_name)
+        clip = mp.VideoFileClip(video_file_path, fps_source="fps")
+        clip.write_videofile(save_path)
+
+
 def main():
+    load_embeddings()
+    # mpg_to_mp4()
     # generate_text_file_from_videos()
+    # copy_en_txt(r"C:\Users\ataul\source\Uni\BachelorThesis\poc\AudioTranscriptsSet1_EN.csv",r"C:\Users\ataul\source\Uni\BachelorThesis\poc\AudioTranscriptsSet1.csv","1")
+    # get_set_files(r"C:\Users\ataul\source\Uni\BachelorThesis\Stimuli_logs\Verstehen_Set1.txt", "1")
 
     rsa_con = RSAConfig()
     rsa_con.strategy = Strategy.mice.name
